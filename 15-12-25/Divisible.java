4a96fd6cc481ee3370828bba6c032168053e0576
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2025-12-15 22:19:12 +0530

    divide by 5 & 11

---
+++ 15-12-25/Divisible.java
@@ -0,0 +1,14 @@
+/*Q35. Write a Java program to check whether a number is divisible by 5 and 11 or not. 
+(Using ternary operator)
+*/
+import java.util.*;
+public class Divisible{
+	public static void main(String x[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number to check number is divisible by 5 or 11");
+		
+		int num = sc.nextInt();
+		String str = num % 5 == 0 && num % 11 ==0 ? "divided by both" : "not devided by both";
+		System.out.println(str	);
+	}
+}
