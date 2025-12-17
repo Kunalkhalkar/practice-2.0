44229082b5d480d18ed9e771ffe82611f3f70c75
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2025-12-17 21:48:19 +0530

    Series 2

---
+++ 15-12-25/Series2.java
@@ -0,0 +1,8 @@
+/*Q51. Write a java program to display following series :
+           	1  2  2  4  3  6  4  8  5  10  6  12
+*/
+public class Series2{
+	public static void main(String ...x){
+		for(int i = 1; i<=6; System.out.print((i) + "\t" + (i*2)+"\t"),i++){}
+	}
+}
