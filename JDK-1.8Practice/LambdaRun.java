300a7f431be909fb4fd5b2530a63034dbcb3bbd3
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-03-24 23:43:02 +0500

    done

---
+++ JDK-1.8Practice/LambdaRun.java
@@ -0,0 +1,24 @@
+/*
+Lambda java 1.8
+*/
+import java.util.*;
+public class LambdaRun{
+	public static void main(String...x){
+		Scanner sc = new Scanner(System.in);
+		
+		Runnable r = () -> {
+			for(int i=0; i<10; i++){
+				
+				System.out.println(i + " ");
+				try{
+				Thread.sleep(1000);
+				}
+				catch(InterruptedException ex){
+				System.out.println(ex);
+				}
+			}
+		};
+		Thread t = new Thread(r);
+		t.run();
+	}
+}
